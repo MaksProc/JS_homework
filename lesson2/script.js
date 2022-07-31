479b1a0cd51ddc4d0b4bc33cf582467da969562e
MaksProc <maks.procenko05@gmail.com>
2022-07-31 21:00:59 +0300

    lesson 2

---
+++ lesson2/script.js
@@ -0,0 +1,70 @@
+arr = ['zero',42,676,'line',true,5435,null,94898298,'word',false]
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
+console.log(arr[5])
+console.log(arr[6])
+console.log(arr[7])
+console.log(arr[8])
+console.log(arr[9])
+console.log(arr)
+
+book1 = {
+    title:'Roses are red',
+    pageCount:999,
+    genre:'philosophy'
+}
+book2 = {
+    title:'Violets are blue',
+    pageCount:2,
+    genre:'song lyrics'
+}
+book1 = {
+    title:'true != false',
+    pageCount:226,
+    genre:'guides'
+}
+
+book11 = {
+    title: 'QWE',
+    pageCount:123,
+    genre: 'meta',
+    authors: [{name: 'Al', age:13},{name: 'Qu', age:16}]
+}
+book12 = {
+    title:'ASD',
+    pageCount:456,
+    genre: 'keyboard',
+    authors:[{name:'Ja',age:32},{name:'Yi',age:46}]
+}
+book13 = {
+    title:'ZXC',
+    pageCount:789,
+    genre: 'genreless',
+    authors:[{name:'Do',age:65},{name:'Nu',age:68}]
+}
+
+uzerz = [
+    {name:'A',username:'Alpha',password:'123'},
+    {name:'B',username:'Bravo',password:'456'},
+    {name:'C',username:'Charlie',password:'789'},
+    {name:'D',username:'Delta',password:'qwe'},
+    {name:'E',username:'Echo',password:'rty'},
+    {name:'F',username:'Foxtrot',password:'uio'},
+    {name:'G',username:'Golf',password:'pas'},
+    {name:'H',username:'Hotel',password:'dfg'},
+    {name:'I',username:'India',password:'hjk'},
+    {name:'J',username:'Juliet',password:'lzx'}
+    ]
+console.log(uzerz[0].password)
+console.log(uzerz[1].password)
+console.log(uzerz[2].password)
+console.log(uzerz[3].password)
+console.log(uzerz[4].password)
+console.log(uzerz[5].password)
+console.log(uzerz[6].password)
+console.log(uzerz[7].password)
+console.log(uzerz[8].password)
+console.log(uzerz[9].password)
